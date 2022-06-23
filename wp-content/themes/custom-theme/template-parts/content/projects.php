16333b8edcd4469fb8f071b88ba0cc9bb72f2817
John Hysong <johnahysong@gmail.com>
2022-06-23 15:35:59 -0400

    made projects map out

--- wp-content/themes/custom-theme/template-parts/content/projects.php
+++ wp-content/themes/custom-theme/template-parts/content/projects.php
@@ -14,7 +14,9 @@
 // args
 $args = [
     'post_type' => ['projects'],
-    'post_status' => ['publish']
+    'post_status' => ['publish'],
+    'posts_per_page' => -1,
+    'order'   => 'ASC',
 ];
 
 // The Query
@@ -22,12 +24,49 @@ $the_query = new WP_Query( $args );
 
 // The Loop
 if ( $the_query->have_posts() ) {
-    echo '<ul>';
+    ?>
+
+<div class="pl">
+      <div class="pl-texts">
+        <h1 class="pl-title">Projects</h1>
+        <p class="pl-desc">
+          Here are some examples of what I've been working on...
+        </p>
+      </div>
+      <div class="pl-list">
+<?php
+
+    // echo '<ul>';
     while ( $the_query->have_posts() ) {
         $the_query->the_post();
-        echo '<li>' . get_the_title() . '</li>';
+        // echo '<li>' . get_the_title() . '</li>';
+        // echo '<li>' . get_the_content() . '</li>';
+    
+    // echo '</ul>';
+    ?>
+    <div class="p">
+      <div class="p-browser">
+        <div class="p-circle-r"></div>
+        <div class="p-circle-y"></div>
+        <div class="p-circle-g"></div>
+      </div>
+
+        <?php 
+            if (has_post_thumbnail()) {
+                ?>
+                <a href="http://johnatticus.com" target="_blank" rel="noreferrer">
+                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="project" class="p-img" />
+                </a>
+                <?php
+            } 
+        ?>
+    </div>
+    <?php
     }
-    echo '</ul>';
+    ?>
+        </div> <!--end pl-list-->
+    </div> <!--end pl-->
+    <?php
 } else {
     echo 'no posts found';
 }
